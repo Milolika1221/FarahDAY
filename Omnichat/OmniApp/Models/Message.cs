bf2bca683ddcf3b237709e013ab2adc423bd8400
Gimserter <ychenick3@gmail.com>
2024-11-02 18:05:00 +0700

    Структуризация проекта

--- Omnichat/OmniApp/Models/Message.cs
+++ Omnichat/OmniApp/Models/Message.cs
@@ -6,23 +6,6 @@ using System.Threading.Tasks;
 
 namespace OmniApp
 {
-    public class Chat
-    {
-        public string Source { get; set; }
-        public string Chat_id { get; set; }
-        public string Status { get; set; }
-        public string Type { get; set; }
-        public string Priority { get; set; }
-        public Chat(string source, string chat_id, string status = null, string type = null, string priority = null)
-        {
-            Source = source;
-            Chat_id = chat_id;
-            Status = status;
-            Type = type;
-            Priority = priority;
-        }
-    }
-
     public class Message
     {
         public string Source { get; set; }
